6072b94fef69000230e91818813c1415b3dd9b54
QuirrelForU <symmlux@gmail.com>
2024-03-27 02:36:48 +0700

    test fixed

--- test/test_chess_move.py
+++ test/test_chess_move.py
@@ -27,13 +27,45 @@ class TestTChessMove(unittest.TestCase):
 
     def test_capture_move(self):
         bishop = Bishop((1, 1), ESide.WHITE)
-        enemy_piece = TChessman(EChessmanType.PAWN, (2, 2), ESide.BLACK)  # Enemy piece acting as a pawn
+        enemy_piece = TChessman(EChessmanType.PAWN, (2, 2), ESide.BLACK)
         self.chessboard.get_field(1, 1).occupied_by = bishop
         self.chessboard.get_field(2, 2).occupied_by = enemy_piece
         move = TChessMove(self.chessboard, bishop, (2, 2))
         move.execute()
         self.assertIsNone(self.chessboard.get_field(1, 1).occupied_by)
         self.assertEqual(self.chessboard.get_field(2, 2).occupied_by, bishop)
-        self.assertIsNone(enemy_piece.position)  # Assuming this is how we mark captured pieces
+        self.assertIsNone(enemy_piece.position)
         self.assertEqual(str(move), 'Bb2xc3')
 
+    def test_rook_capture_move(self):
+        rook = Rook((1, 0), ESide.WHITE)
+        enemy_piece = TChessman(EChessmanType.PAWN, (1, 5), ESide.BLACK)
+        self.chessboard.get_field(1, 0).occupied_by = rook
+        self.chessboard.get_field(1, 5).occupied_by = enemy_piece
+        move = TChessMove(self.chessboard, rook, (1, 5))
+        move.execute()
+        self.assertIsNone(self.chessboard.get_field(1, 0).occupied_by)
+        self.assertEqual(self.chessboard.get_field(1, 5).occupied_by, rook)
+        self.assertIsNone(enemy_piece.position)
+        self.assertEqual(str(move), 'Rb1xb6')
+
+    def test_rook_move_to_empty_square(self):
+        rook = Rook((7, 7), ESide.WHITE)
+        self.chessboard.get_field(7, 7).occupied_by = rook
+        move = TChessMove(self.chessboard, rook, (7, 4))
+        move.execute()
+        self.assertIsNone(self.chessboard.get_field(7, 7).occupied_by)
+        self.assertEqual(self.chessboard.get_field(7, 4).occupied_by, rook)
+        self.assertEqual(str(move), 'Rh8h5')
+
+    def test_bishop_diagonal_capture(self):
+        bishop = Bishop((2, 0), ESide.WHITE)
+        enemy_piece = TChessman(EChessmanType.PAWN, (4, 2), ESide.BLACK)
+        self.chessboard.get_field(2, 0).occupied_by = bishop
+        self.chessboard.get_field(4, 2).occupied_by = enemy_piece
+        move = TChessMove(self.chessboard, bishop, (4, 2))
+        move.execute()
+        self.assertIsNone(self.chessboard.get_field(2, 0).occupied_by)
+        self.assertEqual(self.chessboard.get_field(4, 2).occupied_by, bishop)
+        self.assertIsNone(enemy_piece.position)
+        self.assertEqual(str(move), 'Bc1xe3')
