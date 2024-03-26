529609b8c47779cf0973a054941bc629120e032e
QuirrelForU <symmlux@gmail.com>
2024-03-27 02:47:27 +0700

    some more annotations

--- GameBoard/chessboard.py
+++ GameBoard/chessboard.py
@@ -2,8 +2,18 @@ from GameBoard.t_chessfield import TChessField
 
 
 class Chessboard:
+    """
+    Represents a chessboard, holding a grid of TChessField objects.
+    """
+
     def __init__(self):
         self.board = [[TChessField(row, col) for col in range(8)] for row in range(8)]
 
-    def get_field(self, row, col):
+    def get_field(self, row: int, col: int) -> TChessField:
+        """
+        Retrieve a specific field from the chessboard.
+        :param row: The row index of the field.
+        :param col: The column index of the field.
+        :return: The TChessField object at the specified location.
+        """
         return self.board[row][col]
--- GameBoard/t_chessfield.py
+++ GameBoard/t_chessfield.py
@@ -1,10 +1,18 @@
-
 class TChessField:
-    def __init__(self, row, col):
+    """
+    Represents a single field on a chessboard, holding information about its position and occupancy.
+    """
+
+    def __init__(self, row: int, col: int):
         self.row = row
         self.col = col
         self.occupied_by = None
 
     @property
-    def is_busy(self):
+    def is_busy(self) -> bool:
+        """
+        Checks if the field is occupied by a chess piece.
+
+        :return: True if the field is occupied, False otherwise.
+        """
         return self.occupied_by is not None
